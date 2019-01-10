c56c2c609dd5a0844bb2c703019315d2939de4a6
Bogdan-Mihai Ionescu <bogdan@ionescu.io>
2019-01-10 12:15:30 +0200

    Add files via upload

---
+++ src/validator-errors.component.js
@@ -0,0 +1,8 @@
+(function () {
+
+angular.module('kv').component('validatorErrors',
+	{
+		templateUrl: '/assets/custom/js/directives/validator-errors/validator-errors.component.html',
+	});
+
+}());
