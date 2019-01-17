a5ffcb24e088b6f0a1a20ed2308a512d6317fe4d
Bogdan-Mihai Ionescu <bogdan@ionescu.io>
2019-01-17 13:00:46 +0200

    Update Controller.php
    add support for related models object, not only for array of objects

--- src/Controller.php
+++ src/Controller.php
@@ -23,14 +23,19 @@ class Controller extends BaseController
 		$model = \App::make('\App\Models\\' . $camelCase);
 		$validators = $model->getValidations();
 		$input = is_array($request) ? $request : $request->all();
-		$validation = Validator::make($input, $validators["rules"], $validators["messages"]);
+		$validation = \Validator::make($input, $validators["rules"], $validators["messages"]);
 		if (method_exists($model, "conditionalValidations")) $model->conditionalValidations($validation);
 		$errors = $validation->fails() ? $validation->errors()->messages() : [];
 		foreach ($model->relatedModels as $relatedModelName) {
 			if (!isset($input[$relatedModelName])) continue;
+			if (isset($input[$relatedModelName][0]))
 			foreach ($input[$relatedModelName] as $index => $item) {
 				$result = $this->validate($relatedModelName, $item);
 				if (isset($result["status"])) $errors[$relatedModelName][] = array("index" => $index, "errors" => $result['error']);
+			}
+			else {
+				$result = $this->validate($relatedModelName, $input[$relatedModelName]);
+				if (isset($result["status"])) $errors[$relatedModelName][] = array("index" => 0, "errors" => $result['error']);
 			}
 		}
 		if (count($errors)) return Helper::setValidationErrorResponse($errors);
