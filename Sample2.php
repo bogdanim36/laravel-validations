89e636a686525230e9cab97d33d7931e4096fb40
Bogdan-Mihai Ionescu <bogdan@ionescu.io>
2019-01-28 10:40:03 +0200

    Update Sample2.php

--- Sample2.php
+++ Sample2.php
@@ -12,7 +12,7 @@ class Sample2 extends BaseModel
      */
     protected $table = 'sample2';
     protected $primaryKey = 'sample2_id';
-    public $relatedModels = ["phone", "email", "address"];
+    public $relatedModels = ["phone"=>"many", "email"=>"many", "address"=>"one"];
 
     protected $validations = [
         "first_name" => "required|min:1|max:100",
